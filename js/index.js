b925e662f83ed81d35373e054ebd3c0561dc6dc5
Benjamin Doll <human@benji.to>
2024-01-22 14:37:32 -0500

    Update product of the day

--- js/index.js
+++ js/index.js
@@ -7,37 +7,38 @@ gtag("js", new Date());
 gtag("config", "G-3WLV6XLM6N");
 
 var products = [
-  "https://www.ebay.ca/itm/185681140136", // 1:
-  "https://www.ebay.ca/itm/185681140136", // 2:
-  "https://www.ebay.ca/itm/185681140136", // 3:
-  "https://www.ebay.ca/itm/185681140136", // 4:
-  "https://www.ebay.ca/itm/185681140136", // 5:
-  "https://www.ebay.ca/itm/185681140136", // 6:
-  "https://www.ebay.ca/itm/185681140136", // 7:
-  "https://www.ebay.ca/itm/185681140136", // 8;
-  "https://www.ebay.ca/itm/185681140136", // 9:
-  "https://www.ebay.ca/itm/185681140136", // 10:
-  "https://www.ebay.ca/itm/185681140136", // 11:
-  "https://www.ebay.ca/itm/185681140136", // 12:
-  "https://www.ebay.ca/itm/185681140136", // 13:
-  "https://www.ebay.ca/itm/185681140136", // 14:
-  "https://www.ebay.ca/itm/185681140136", // 15:
-  "https://www.ebay.ca/itm/185681140136", // 16:
-  "https://www.ebay.ca/itm/185681140136", // 17:
-  "https://www.ebay.ca/itm/185681140136", // 18:
-  "https://www.ebay.ca/itm/185681140136", // 19:
-  "https://www.ebay.ca/itm/185681140136", // 20:
-  "https://www.ebay.ca/itm/185681140136", // 21:
-  "https://www.ebay.ca/itm/185681140136", // 22:
-  "https://www.ebay.ca/itm/185681140136", // 23:
-  "https://www.ebay.ca/itm/185681140136", // 24:
-  "https://www.ebay.ca/itm/185681140136", // 25:
-  "https://www.ebay.ca/itm/185681140136", // 26:
-  "https://www.ebay.ca/itm/185681140136", // 27:
-  "https://www.ebay.ca/itm/185681140136", // 28:
-  "https://www.ebay.ca/itm/185681140136", // 29:
-  "https://www.ebay.ca/itm/185681140136", // 30:
-  "https://www.ebay.ca/itm/185681140136", // 31:
+  "https://www.ebay.ca/itm/333200896831", // 1:
+  "https://www.ebay.ca/itm/333200896831", // 2:
+  "https://www.ebay.ca/itm/333200896831", // 3:
+  "https://www.ebay.ca/itm/333200896831", // 4:
+  "https://www.ebay.ca/itm/333200896831", // 5:
+  "https://www.ebay.ca/itm/333200896831", // 6:
+  "https://www.ebay.ca/itm/333200896831", // 7:
+  "https://www.ebay.ca/itm/333200896831", // 8;
+  "https://www.ebay.ca/itm/333200896831", // 9:
+  "https://www.ebay.ca/itm/333200896831", // 10:
+  "https://www.ebay.ca/itm/333200896831", // 11:
+  "https://www.ebay.ca/itm/333200896831", // 12:
+  "https://www.ebay.ca/itm/333200896831", // 13:
+  "https://www.ebay.ca/itm/333200896831", // 14:
+  "https://www.ebay.ca/itm/333200896831", // 15:
+  "https://www.ebay.ca/itm/333200896831", // 16:
+  "https://www.ebay.ca/itm/333200896831", // 17:
+  "https://www.ebay.ca/itm/333200896831", // 18:
+  "https://www.ebay.ca/itm/333200896831", // 19:
+  "https://www.ebay.ca/itm/333200896831", // 20:
+  "https://www.ebay.ca/itm/333200896831", // 21:
+  "https://www.ebay.ca/itm/333200896831", // 22:
+  "https://www.ebay.ca/itm/333200896831", // 23:
+  "https://www.ebay.ca/itm/333200896831", // 24:
+  "https://www.ebay.ca/itm/333200896831", // 25:
+  "https://www.ebay.ca/itm/333200896831", // 26:
+  "https://www.ebay.ca/itm/333200896831", // 27:
+  "https://www.ebay.ca/itm/333200896831", // 28:
+  "https://www.ebay.ca/itm/333200896831", // 29:
+  "https://www.ebay.ca/itm/333200896831", // 30:
+  "https://www.ebay.ca/itm/333200896831", // 31:
+  // "https://www.ebay.ca/itm/333200896831", // chili
 ];
 
 window.onload = function startUp() {
